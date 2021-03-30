8b0047a858930601758ad949c1eb20619ac2e4e9
Carlos Eduardo Zanco Batistão <carlosz@ciandt.com>
2021-03-30 08:35:45 -0300

    ajusting jsonproperty return name from data envelope

--- food-restaurant/src/main/kotlin/com/food/restaurant/entrypoint/rest/json/DataResponse.kt
+++ food-restaurant/src/main/kotlin/com/food/restaurant/entrypoint/rest/json/DataResponse.kt
@@ -3,6 +3,6 @@ package com.food.restaurant.entrypoint.rest.json
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class DataResponse<T>(
-        @JsonProperty("data") val t: T
+        @get:JsonProperty("data") val t: T
 ) {
 }
